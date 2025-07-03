8be018de39bbef35bca07f27d46e8bea36e1299e
Glenn McCrea <glennmccrea@icloud.com>
2025-07-02 19:22:23 -0500

    init

---
+++ main.js
@@ -0,0 +1,40 @@
+/* String to Use
+
+let inputString = " Welcome to the Coding Bootcamp! Learn JavaScript
+today. ";
+
+Tasks
+Complete the following tasks and assign the results to the specified variables. Log
+each result to the console.
+1. Searching
+    1. Check if the word "JavaScript" is in the string using includes and assign
+    the result to a variable named hasJavaScript.
+    2. Find the position of the word "Coding" in the string using indexOf and
+    assign the result to a variable named codingPosition.
+    3. Check if the string starts with "Welcome" using startsWith and assign the
+    result to a variable named startsWithWelcome.
+    4. Check if the string ends with "today." using endsWith and assign the result
+    to a variable named endsWithToday.
+
+2. Transforming
+    1. Convert the string to all lowercase letters using toLowerCase and assign the
+    result to a variable named lowercaseString.
+    2. Convert the string to all uppercase letters using toUpperCase and assign the
+    result to a variable named uppercaseString.
+    1
+    3. Remove the extra spaces from the beginning and end of the string using
+    trim and assign the result to a variable named trimmedString.
+    4. Replace the word "JavaScript" with "coding" using replace and assign
+    the result to a variable named replacedString.
+
+3. Breaking Apart
+    1. Split the string into an array of words using split with a space (" ") as the
+    delimiter and assign the result to a variable named wordsArray.
+4. Retrieving
+    1. Retrieve the first character of the trimmed string using charAt and assign
+    the result to a variable named firstCharacter.
+    2. Extract the word "Bootcamp" from the string using slice and assign the
+    result to a variable named extractedBootcamp. */
+
+    let inputString = " Welcome to the Coding Bootcamp! Learn JavaScript
+today.
